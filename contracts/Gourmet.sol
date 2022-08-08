81e5bb50a0df13f569c16749ba83b4dbc2aaf459
MIYABETaisei <mytspe46@gmail.com>
2022-08-08 22:16:26 +0900

    🚧 update contract

--- contracts/Gourmet.sol
+++ contracts/Gourmet.sol
@@ -4,21 +4,25 @@ pragma solidity ^0.8.9;
 contract Gourmet {
     struct Info {
         string name;
+        int256 latitude;
+        int256 longitude;
+        uint256 star;
     }
+
+    event NewMap(string name, int256 latitude, int256 longitude, uint256 star);
     mapping(address => Info) public eval;
-    mapping(address => bool) public inserted;
-    address[] public keys;
 
-    function showStar() external view returns (Info memory) {
-        return eval[msg.sender];
-    }
-
-    function setInfo(string memory _name) external {
-        eval[msg.sender].name = _name;
-
-        if (!inserted[msg.sender]) {
-            inserted[msg.sender] = true;
-            keys.push(msg.sender);
-        }
+    function addMap(
+        string calldata _name,
+        int256 _latitude,
+        int256 _longitude,
+        uint256 _star
+    ) external {
+        Info storage _eval = eval[msg.sender];
+        _eval.name = _name;
+        _eval.latitude = _latitude;
+        _eval.longitude = _longitude;
+        _eval.star = _star;
+        emit NewMap(_name, _latitude, _longitude, _star);
     }
 }
