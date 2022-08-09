f5dba6debb6e0279aaee32bd1ff9f5ec56d22665
MIYABETaisei <mytspe46@gmail.com>
2022-08-09 22:17:55 +0900

    ⚡️

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,14 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  paths: {
+    artifacts: "./artifacts",
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+  },
 };
 
 export default config;
