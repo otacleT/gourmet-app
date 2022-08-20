2302121f6505382068952e329bedab1880950493
MIYABETaisei <mytspe46@gmail.com>
2022-08-20 21:15:40 +0900

    ⚡️ カテゴリー、住所追加

--- contracts/Gourmet.sol
+++ contracts/Gourmet.sol
@@ -4,25 +4,45 @@ pragma solidity ^0.8.9;
 contract Gourmet {
     struct Info {
         string name;
+        string category;
+        string address_ja;
         int256 latitude;
         int256 longitude;
         uint256 star;
     }
 
-    event NewMap(string name, int256 latitude, int256 longitude, uint256 star);
+    event NewMap(
+        string name,
+        string category,
+        string address_ja,
+        int256 latitude,
+        int256 longitude,
+        uint256 star
+    );
     mapping(address => Info) public eval;
 
     function addMap(
         string calldata _name,
+        string calldata _category,
+        string calldata _address_ja,
         int256 _latitude,
         int256 _longitude,
         uint256 _star
     ) external {
         Info storage _eval = eval[msg.sender];
         _eval.name = _name;
+        _eval.category = _category;
+        _eval.address_ja = _address_ja;
         _eval.latitude = _latitude;
         _eval.longitude = _longitude;
         _eval.star = _star;
-        emit NewMap(_name, _latitude, _longitude, _star);
+        emit NewMap(
+            _name,
+            _category,
+            _address_ja,
+            _latitude,
+            _longitude,
+            _star
+        );
     }
 }
