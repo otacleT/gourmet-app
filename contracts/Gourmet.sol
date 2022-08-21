a0e7ef5c1e6157f34598c05b49b81ded73c89598
MIYABETaisei <mytspe46@gmail.com>
2022-08-21 19:58:26 +0900

    ✨ お店の評価を行う

--- contracts/Gourmet.sol
+++ contracts/Gourmet.sol
@@ -2,47 +2,17 @@
 pragma solidity ^0.8.9;
 
 contract Gourmet {
-    struct Info {
-        string name;
-        string category;
-        string address_ja;
-        int256 latitude;
-        int256 longitude;
-        uint256 star;
-    }
+    mapping(uint256 => uint256) star;
+    event starLog(uint256 result);
 
-    event NewMap(
-        string name,
-        string category,
-        string address_ja,
-        int256 latitude,
-        int256 longitude,
-        uint256 star
-    );
-    mapping(address => Info) public eval;
-
-    function addMap(
-        string calldata _name,
-        string calldata _category,
-        string calldata _address_ja,
-        int256 _latitude,
-        int256 _longitude,
-        uint256 _star
-    ) external {
-        Info storage _eval = eval[msg.sender];
-        _eval.name = _name;
-        _eval.category = _category;
-        _eval.address_ja = _address_ja;
-        _eval.latitude = _latitude;
-        _eval.longitude = _longitude;
-        _eval.star = _star;
-        emit NewMap(
-            _name,
-            _category,
-            _address_ja,
-            _latitude,
-            _longitude,
-            _star
-        );
+    function Evaluate(uint256 _uStar, uint256 _shopId) external {
+        uint256 result;
+        if (star[_shopId] == 0) {
+            result = _uStar;
+        } else {
+            result = (star[_shopId] + _uStar) / 2;
+        }
+        star[_shopId] = result;
+        emit starLog(result);
     }
 }
