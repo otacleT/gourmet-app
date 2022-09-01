c0cb197dc770783fe61a8a88d007d8220cf1666a
MIYABETaisei <mytspe46@gmail.com>
2022-09-01 22:18:02 +0900

    ✨ Goerliテストネットワークへデプロイ

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,22 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+const networks: { [key: string]: any } = {
+  goerli: {
+    url: process.env.ALCHEMY_GOERLI_URL,
+    accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+  },
+};
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   paths: {
     artifacts: "./artifacts",
   },
-  networks: {
-    hardhat: {
-      chainId: 31337,
-    },
-  },
+  networks,
+  defaultNetwork: "goerli",
 };
 
 export default config;
